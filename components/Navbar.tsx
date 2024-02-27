64cbcbd28320c8faac7fc0747593e8e8bd314066
thepalestiniannft <thepalestinian.nft@gmail.com>
2024-02-27 14:56:07 -0500

    update links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
         <div className={styles.container}>
             <div className={styles.navbar}>
                 <div className={styles.redlogo}>
-                    <a href="https://thepalestinians.morrlabs.xyz">
+                    <a href="https://thepalestiniannft.github.io/home/">
                         <Image
                             src={logo}
                             alt="logo"
